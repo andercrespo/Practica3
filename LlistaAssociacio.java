1109760c57134b44e7d12bccd635917b02ca9f79
oscarfuga23 <oscar.fumado@estudiants.urv.cat>
2025-01-07 22:36:38 +0100

    LlistaAssociacio.java

--- LlistaAssociacio.java
+++ LlistaAssociacio.java
@@ -7,33 +7,61 @@ public class LlistaAssociacio {
     private int capacitat;
     private int tamany;
 
-    //Constructor
-    public LlistaAssociacio (int capacitat){
+    /**
+     * Constructor de la classe LlistaAssociacio.
+     * 
+     * @param capacitat la capacitat inicial de la llista.
+     */
+    public LlistaAssociacio(int capacitat) {
         this.associacions = new Associacio[capacitat];
-        this.capacitat= capacitat;
-        this.tamany=0;
+        this.capacitat = capacitat;
+        this.tamany = 0;
     }
 
-    public int getTamany(){
+    /**
+     * Obté el nombre d'associacions actuals en la llista.
+     * 
+     * @return el tamany actual de la llista.
+     */
+    public int getTamany() {
         return tamany;
     }
 
-    public Associacio[] getAssociacions(){
+    /**
+     * Obté l'array d'associacions actuals.
+     * 
+     * @return un array d'objectes Associacio.
+     */
+    public Associacio[] getAssociacions() {
         return associacions;
     }
 
-    public void setAssociacions(Associacio[] associacions){
+    /**
+     * Assigna un nou array d'associacions a la llista.
+     * 
+     * @param associacions l'array d'associacions a assignar.
+     */
+    public void setAssociacions(Associacio[] associacions) {
         this.associacions = associacions;
     }
 
-    public void aumentaTamany(){
+    /**
+     * Incrementa el tamany de la llista en una unitat.
+     */
+    public void aumentaTamany() {
         tamany++;
     }
 
-    public void afegirAssociacio(Associacio associacio){
-        if(capacitat==tamany){
-            capacitat=capacitat*2;
-            Associacio[] newAssociacions= new Associacio[capacitat];
+    /**
+     * Afegeix una nova associació a la llista.
+     * Si la capacitat actual es completa, es duplica la mida de la llista.
+     * 
+     * @param associacio l'Associacio a afegir.
+     */
+    public void afegirAssociacio(Associacio associacio) {
+        if (capacitat == tamany) {
+            capacitat = capacitat * 2;
+            Associacio[] newAssociacions = new Associacio[capacitat];
             System.arraycopy(associacions, 0, newAssociacions, 0, tamany);
             associacions = newAssociacions;
         }
@@ -41,43 +69,62 @@ public class LlistaAssociacio {
         aumentaTamany();
     }
 
-    public boolean eliminarAssociacio(String nom){
+    /**
+     * Elimina una associació de la llista basada en el seu nom.
+     * 
+     * @param nom el nom de l'associació a eliminar.
+     * @return true si l'associació es va eliminar correctament, false altrament.
+     */
+    public boolean eliminarAssociacio(String nom) {
         boolean eliminat = false;
         int i = 0;
-        while(!eliminat && i < tamany){
-            if(associacions[i].getNom().equalsIgnoreCase(nom)){
-                for(int j = i; j < tamany; j++){
-                    associacions[j]=associacions[j+1];
+        while (!eliminat && i < tamany) {
+            if (associacions[i].getNom().equalsIgnoreCase(nom)) {
+                for (int j = i; j < tamany - 1; j++) {
+                    associacions[j] = associacions[j + 1];
                 }
+                associacions[tamany - 1] = null;
                 eliminat = true;
-            }
-            else{
+            } else {
                 i++;
             }
         }
-        tamany--;
+        if (eliminat) {
+            tamany--;
+        }
         return eliminat;
     }
 
-    public Associacio buscarAssociacio(String nom){
+    /**
+     * Cerca una associació en la llista pel seu nom.
+     * 
+     * @param nom el nom de l'associació a buscar.
+     * @return l'Associacio trobada o null si no existeix.
+     */
+    public Associacio buscarAssociacio(String nom) {
         boolean trobat = false;
         int i = 0;
-        while(!trobat && i < tamany){
-            if(associacions[i].getNom().equalsIgnoreCase(nom)){
+        while (!trobat && i < tamany) {
+            if (associacions[i].getNom().equalsIgnoreCase(nom)) {
                 return associacions[i];
-            }
-            else{
+            } else {
                 i++;
             }
         }
         return null;
     }
 
-    public Associacio[] obtenirAssociacionsAmbTitulacions(String titulacio){
+    /**
+     * Obté les associacions que tenen una titulació específica.
+     * 
+     * @param titulacio la titulació a buscar.
+     * @return un array d'associacions que compleixen el criteri.
+     */
+    public Associacio[] obtenirAssociacionsAmbTitulacions(String titulacio) {
         int contador = 0;
         for (int i = 0; i < tamany; i++) {
-            for (String titulacioAssociacio : associacions[i].getTitulacions()){
-                if (titulacioAssociacio.equalsIgnoreCase(titulacio)){
+            for (String titulacioAssociacio : associacions[i].getTitulacions()) {
+                if (titulacioAssociacio.equalsIgnoreCase(titulacio)) {
                     contador++;
                     break;
                 }
@@ -98,14 +145,18 @@ public class LlistaAssociacio {
         return associacionsTrobades;
     }
 
-    public String obtenirInformacio(){
+    /**
+     * Genera una representació textual de totes les associacions en la llista.
+     * 
+     * @return una cadena amb informació de totes les associacions.
+     */
+    public String obtenirInformacio() {
         String[] associacionsString = new String[tamany];
-        for(int i=0; i<tamany;i++){
-            associacionsString[i]=associacions[i].getNom();
+        for (int i = 0; i < tamany; i++) {
+            associacionsString[i] = associacions[i].getNom();
         }
         String stringAssociacions = String.join("\n", associacionsString);
         stringAssociacions = String.join("Aquesta es la llista d'associacions:\n ", stringAssociacions);
         return stringAssociacions;
     }
-
 }
